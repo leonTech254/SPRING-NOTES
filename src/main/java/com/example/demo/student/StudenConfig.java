dd99cacaecdc2267c5a05ef9fc40cacbc939ff89
leonTech254 <martinleontech23@gmail.com>
2023-05-05 17:18:20 +0300

    --post-data

--- src/main/java/com/example/demo/student/StudenConfig.java
+++ src/main/java/com/example/demo/student/StudenConfig.java
@@ -1,33 +1,33 @@
-package com.example.demo.student;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.util.List;
-
-@Configuration
-public class StudenConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(StudentsRespostory respostory)
-    {
-        return args -> {
-            Student martin=new Student(
-                    "martin",
-                    "martin@gmail.com",
-                    LocalDate.of(2023,5,2)
-            );
-            Student leon=new Student(
-
-                    "leon",
-                    "leon@gmail.com",
-                    LocalDate.of(2000,5,2)
-            );
-            respostory.saveAll(
-                    List.of(martin,leon)
-            );
-        };
-
-    }
-}
+//package com.example.demo.student;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@Configuration
+//public class StudenConfig {
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentsRespostory respostory)
+//    {
+//        return args -> {
+//            Student martin=new Student(
+//                    "martin",
+//                    "martin@gmail.com",
+//                    LocalDate.of(2023,5,2)
+//            );
+//            Student leon=new Student(
+//
+//                    "leon",
+//                    "leon@gmail.com",
+//                    LocalDate.of(2000,5,2)
+//            );
+//            respostory.saveAll(
+//                    List.of(martin,leon)
+//            );
+//        };
+//
+//    }
+//}
